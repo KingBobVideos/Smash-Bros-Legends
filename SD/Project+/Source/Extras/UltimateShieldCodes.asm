4cfdaeda3630072cc17732aa9f715adefee3dfda
Grimdeath99 <65681184+Grimdeath99@users.noreply.github.com>
2021-06-27 22:53:03 +0200

    Adding Support for Mem Expansion + Other Stuff

---
+++ SD/Project+/Source/Extras/UltimateShieldCodes.asm
@@ -0,0 +1,9 @@
+#############################################################
+Ultimate Shield Stats v0.1 [Grimdeath, Snoe]
+#############################################################
+3234 - Shield Drain Per Frame [0.15 Legends] [Grimdeath]
+    float 0.15     @ $80B88450
+3235 - Shield Gain Per Frame [0.08 Legends] [Grimdeath]
+    float 0.08     @ $80B88454
+3236 - Shield Reset [37.5 Legends] [Grimdeath]
+    float 37.5     @ $80b88458
