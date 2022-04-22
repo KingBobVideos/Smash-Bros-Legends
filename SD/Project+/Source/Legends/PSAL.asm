e538f84037c486bec6a9d0783a2748c8b4420bf1
Grimdeath99 <65681184+Grimdeath99@users.noreply.github.com>
2022-04-22 21:56:53 +0200

    [Code] New PSA code

---
+++ SD/Project+/Source/Legends/PSAL.asm
@@ -0,0 +1,39 @@
+PSA command 11210100 sets next GFX anim index [Eon]
+HOOK @ $807A55F0
+{
+    li r3, 0
+    bne %end%
+    lwz r12, 0(r21)
+    mr r4, r21
+    addi r3, r1, 0x818
+    lwz r12, 0x1C(r12)
+    mtctr r12 
+    bctrl 
+    addi r3, r1, 0x818
+    stw r31, 0x10(r3) #store moduleAccesser for getInt to use
+    li r4, 0
+    #soAnimCmdArgList.getInt(animArgList, index) #static function
+    lis r12, 0x8077 
+    ori r12, r12, 0xDFDC
+    mtctr r12 
+    bctrl
+    mr r27, r3
+
+    #ecMgr.getInstance() #static function
+    lis r12, 0x807A
+    ori r12, r12, 0x2874
+    mtctr r12 
+    bctrl
+    mr r4, r27
+    mr r5, r27
+    mr r6, r27
+    mr r7, r27
+    mr r8, r27
+    mr r9, r27
+    #ecMgr.presetAnimIdx(i, i, i, i, i, i, i) #static function
+    lis r12, 0x8006
+    ori r12, r12, 0x06A8
+    mtctr r12 
+    bctrl
+    li r3, 1
+}
