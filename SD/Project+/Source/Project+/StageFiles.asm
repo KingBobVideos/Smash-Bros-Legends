4e88f276118cfcc623c08cf348163eb542b3842e
vs-toad <koronakos.nicholas@gmail.com>
2022-05-06 12:08:06 -0500

    SSS Edits
    Page 1 stage properly load in.
    Internal filenames changed.
    Cosmetics mixed up for now...

--- SD/Project+/Source/Project+/StageFiles.asm
+++ SD/Project+/Source/Project+/StageFiles.asm
@@ -1254,50 +1254,50 @@ TABLE_1:
 0x24, | # Peach's Castle 64
 0x06, | # Kongo Jungle
 0x09, | # Hyrule Castle
+0x20, | # Super Happy Tree
+0x05, | # Planet Zebes
 0x23, | # Dream Land
+0x25, | # Sector Z
+0x0F, | # Saffron City
 0x3F, | # Mushroom Kingdom 64
+0x21, | # The Beginning
 0x04, | # Metal Cavern
-0x0E, | # Lylat Cruise
-0x0C, | # Yoshi's Island
-0x05, | # Bowser's Castle
-0x21, | # Golden Temple
-0x1E, | # Sky Sanctuary Zone
-0x14, | # Castle Siege
-0x15, | # Wario Land
-0x18, | # Fountain of Dreams
-0x0A, | # Metroid Lab
-0x20, | # Yoshi's Story
-0x1C, | # Green Hill Zone
-0x1A, | # Smashville
-0x28, | # Pokemon Stadium 2
-0x02, | # Delfino's Secret
-0x16, | # Distant Planet
-0x3C, | # TEMP
-0x3D, | # TEMP
-0x3E, | # TEMP
-0x40, | # TEMP
-0x41, | # TEMP
-0x42, | # TEMP
-0x43  | # TEMP
+0x14, | # Peach's Castle
+0x3B, | # Rainbow Cruise
+0x18, | # Kongo Falls
+0x33, | # Jungle Japes
+0x1E, | # Great Bay
+0x1F, | # Hyrule Temple
+0x27, | # Brinstar
+0x31, | # Dinosaur Land
+0x02, | # Yoshi's Story
+0x16, | # Fountain of Dreams
+0x3C, | # Green Greens
+0x3D, | # Corneria
+0x3E, | # Venom
+0x40, | # Pokemon Stadium
+0x22, | # Onett
+0x19, | # Fourside
+0x43  | # Brinstar Depths
 
 TABLE_2:
 	byte[30] |
 0x12, | # Infinite Glacier
 0x1B, | # Shadow Moses Island
-0x19, | # Fourside
-0x22, | # Onett
+0x42, | # TEMP
+0x41, | # TEMP
 0x26, | # Big Blue
 0x11, | # Port Town Aero Dive
 0x13, | # Flat Zone 2
 0x03, | # Luigi's Mansion
 0x07, | # Rumble Falls
-0x25, | # Corneria
-0x27, | # Brinstar
-0x1F, | # Temple
+0x0E, | # Lylat Cruise
+0x1A, | # Smashville
+0x1C, | # Green Hill Zone
 0x08, | # Pirate Ship
 0x2B, | # Training Room
 0x10, | # Spear Pillar
-0x0F, | # Saffron City
+0x0C, | # Yoshi's Island
 0x0D, | # Halberd
 0x0B, | # Frigate Orpheon
 0x17, | # Skyworld
@@ -1315,12 +1315,12 @@ TABLE_2:
 
 TABLE_3:
 	byte[30] |
-0x31, | # Dinosaur Land
+0x28, | # Pokemon Stadium 2
 0x2D, | # Mario Circuit
 0x38, | # Mushroom Kingdom
-0x3B, | # Rainbow Cruise
+0x15, | # Wario Land
 0x32, | # Oil Drum Alley
-0x33, | # Jungle Japes
+0x0A, | # Metroid Lab
 0x2E, | # Clock Town
 0x36, | # Cookie Country
 0x39, | # WarioWare, Inc.
@@ -1353,24 +1353,24 @@ TABLE_STAGES:
 # Table of icon<->stage slot associations
 half[92] |	# Stage Count + 2
 | # OLD SLOTS
-0x0101, 0x0202, 0x0303, 0x0404, | # Battlefield, Final Destination, Delfino's Secret, Luigi's Mansion
-0x0505, 0x0606, 0x0707, 0x0808, | # Metal Cavern, Bowser's Castle, Kongo Jungle, Rumble Falls
+0x0101, 0x0202, 0x0303, 0x0404, | # Battlefield, Final Destination, Yoshi's Story, Luigi's Mansion
+0x0505, 0x0606, 0x0707, 0x0808, | # Metal Cavern, Planet Zebes, Kongo Jungle, Rumble Falls
 0x0909, 0x330A, 0x492C, 0x0C0C, | # Pirate Ship, Hyrule Castle, Metroid Lab, Frigate Orpheon
 0x0D0D, 0x0E0E, 0x130F, 0x1410, | # Yoshi's Island, Halberd, Lylat Cruise, Saffron City
 0x1511, 0x1612, 0x1713, 0x1814, | # Spear Pillar, Port Town Aero Dive, Infinite Glacier, Flat Zone 2
-0x1915, 0x1C16, 0x1D17, 0x1E18, | # Castle Siege, Wario Land, Distant Planet, Skyworld
-0x1F19, 0x201A, 0x211B, 0x221C, | # Fountain of Dreams, Fourside, Smashville, Shadow Moses Island
-0x231D, 0x241E, 0x4326, 0x2932, | # Green Hill Zone, PictoChat, Sky Sanctuary, Temple
-0x2A33, 0x472A, 0x2C35, 0x2D36, | # Yoshi's Story, Golden Temple, Onett, Dream Land
-0x2F37, 0x3038, 0x3139, 0x323A, | # Peach's Castle 64, Corneria, Big Blue, Brinstar
+0x1915, 0x1C16, 0x1D17, 0x1E18, | # Peach's Castle, Wario Land, Fountain of Dreams, Skyworld
+0x1F19, 0x201A, 0x211B, 0x221C, | # Kongo Falls, Fourside, Smashville, Shadow Moses Island
+0x231D, 0x241E, 0x4326, 0x2932, | # Green Hill Zone, PictoChat, Great Bay, Hyrule Temple
+0x2A33, 0x472A, 0x2C35, 0x2D36, | # Super Happy Tree, The Beginning, Onett, Dream Land
+0x2F37, 0x3038, 0x3139, 0x323A, | # Peach's Castle 64, Sector Z, Big Blue, Brinstar
 0x2E3B, 0xFF64, 0xFF64, 0x373C, | # Pokemon Stadium 2, NOTHING, NOTHING, Training Room
 | # NEW SLOTS
 0x4023, 0x4124, 0x4225, 0x251F, | # Dracula's Castle, Mario Circuit, Clock Town, Hanenbow
 0x4427, 0x4528, 0x4629, 0x2B34, | # Dead Line, Dinosaur Land, Oil Drum Alley, Jungle Japes
 0x482B, 0x0B0B, 0x4A2D, 0x4B2E, | # Bell Tower, Norfair, Cookie Country, Venus Lighthouse
 0x4C2F, 0x4D30, 0x4E31, 0x4F3D, | # Mushroom Kingdom, WarioWare, Subspace, Rainbow Cruise
-0x503E, 0x513F,	0x5240, 0x5341,	| # Poke Floats, Mushroom Kingdom 64, TEMP, TEMP
-0x5442, 0x5543, 0x5644, 0x5745,	| # TEMP, TEMP, TEMP, TEMP
+0x503E, 0x513F,	0x5240, 0x5341,	| # Green Greens, Corneria, Venom, Mushroom Kingdom 64
+0x5442, 0x5543, 0x5644, 0x5745,	| # Pokemon Stadium, TEMP, TEMP, Brinstar Depths
 0x5846, 0x5947, 0x5A48, 0x5B49,	| # TEMP, TEMP, TEMP, TEMP
 0x5C4A, 0x5D4B, 0x5E4C, 0x5F4D,	| # TEMP, TEMP, TEMP, TEMP
 0x604E, 0x614F, 0x6250, 0x6351,	| # TEMP, TEMP, TEMP, TEMP
