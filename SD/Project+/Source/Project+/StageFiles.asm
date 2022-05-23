71861fecddf88a613a3e4e08d392a192b4e9a35d
vs-toad <koronakos.nicholas@gmail.com>
2022-05-23 11:22:26 -0500

    All of Page 2 loads now

--- SD/Project+/Source/Project+/StageFiles.asm
+++ SD/Project+/Source/Project+/StageFiles.asm
@@ -1282,40 +1282,40 @@ TABLE_1:
 
 TABLE_2:
 	byte[30] |
-0x12, | # Infinite Glacier
-0x1B, | # Shadow Moses Island
+0x12, | # Mushroom Kingdom Melee
+0x44, | # TEMP
 0x42, | # TEMP
-0x41, | # TEMP
 0x26, | # Big Blue
-0x11, | # Port Town Aero Dive
-0x13, | # Flat Zone 2
-0x03, | # Luigi's Mansion
-0x07, | # Rumble Falls
-0x0E, | # Lylat Cruise
-0x1A, | # Smashville
-0x1C, | # Green Hill Zone
-0x08, | # Pirate Ship
+0x4B, | # Poke Floats
 0x2B, | # Training Room
-0x10, | # Spear Pillar
+0x13, | # Flat Zone 2
+0x45, | # TEMP
+0x39, | # WarioWare, Inc.
+0x47, | # TEMP
+0x35, | # Norfair
+0x0B, | # Frigate Orpheon
 0x0C, | # Yoshi's Island
 0x0D, | # Halberd
-0x0B, | # Frigate Orpheon
+0x0E, | # Lylat Cruise
+0x28, | # Pokemon Stadium 2
+0x11, | # Port Town Aero Dive
+0x46, | # TEMP
+0x59, | # TEMP
+0x1A, | # Smashville
+0x5A, | # TEMP
+0x5B, | # TEMP
 0x17, | # Skyworld
-0x1D, | # PictoChat
-0x44, | # TEMP
-0x45, | # TEMP
-0x46, | # TEMP
-0x44, | # TEMP
-0x45, | # TEMP
-0x46, | # TEMP
-0x47, | # TEMP
+0x1B, | # Shadow Moses Island
+0x03, | # Luigi's Mansion
+0x08, | # Pirate Ship
+0x10, | # Spear Pillar
 0x48, | # TEMP
 0x49, | # TEMP
-0x4A  | # TEMP
+0x2F  | # Hanenbow
 
 TABLE_3:
 	byte[30] |
-0x28, | # Pokemon Stadium 2
+0x1C, | # Green Hill Zone
 0x2D, | # Mario Circuit
 0x38, | # Mushroom Kingdom
 0x15, | # Wario Land
@@ -1323,11 +1323,11 @@ TABLE_3:
 0x0A, | # Metroid Lab
 0x2E, | # Clock Town
 0x36, | # Cookie Country
-0x39, | # WarioWare, Inc.
-0x3C, | # Poke Floats
+0x07, | # Rumble Falls
+0x41, | # TEMP
 0x34, | # Bell Tower
-0x35, | # Norfair
-0x2F, | # Hanenbow
+0x1D, | # PictoChat
+0x4A, | # TEMP
 0x37, | # Venus Lighthouse
 0x2C, | # Dracula's Castle
 0x30, | # Dead Line
@@ -1358,21 +1358,21 @@ half[92] |	# Stage Count + 2
 0x0909, 0x330A, 0x492C, 0x0C0C, | # Pirate Ship, Hyrule Castle, Metroid Lab, Frigate Orpheon
 0x0D0D, 0x0E0E, 0x130F, 0x1410, | # Yoshi's Island, Halberd, Lylat Cruise, Saffron City
 0x1511, 0x1612, 0x1713, 0x1814, | # Spear Pillar, Port Town Aero Dive, Infinite Glacier, Flat Zone 2
-0x1915, 0x1C16, 0x1D17, 0x1E18, | # Peach's Castle, Wario Land, Fountain of Dreams, Skyworld
+0x5A48, 0x1C16, 0x1D17, 0x1E18, | # Peach's Castle, Wario Land, Fountain of Dreams, Skyworld
 0x1F19, 0x201A, 0x211B, 0x221C, | # Kongo Falls, Fourside, Smashville, Shadow Moses Island
 0x231D, 0x241E, 0x4326, 0x2932, | # Green Hill Zone, PictoChat, Great Bay, Hyrule Temple
 0x2A33, 0x472A, 0x2C35, 0x2D36, | # Super Happy Tree, The Beginning, Onett, Dream Land
 0x2F37, 0x3038, 0x3139, 0x323A, | # Peach's Castle 64, Sector Z, Big Blue, Brinstar
 0x2E3B, 0xFF64, 0xFF64, 0x373C, | # Pokemon Stadium 2, NOTHING, NOTHING, Training Room
 | # NEW SLOTS
-0x4023, 0x4124, 0x4225, 0x251F, | # Dracula's Castle, Mario Circuit, Clock Town, Hanenbow
+0x4023, 0x5947, 0x4225, 0x251F, | # Dracula's Castle, TEMP, Clock Town, Hanenbow
 0x4427, 0x4528, 0x4629, 0x2B34, | # Dead Line, Dinosaur Land, Oil Drum Alley, Jungle Japes
 0x482B, 0x0B0B, 0x4A2D, 0x4B2E, | # Bell Tower, Norfair, Cookie Country, Venus Lighthouse
 0x4C2F, 0x4D30, 0x4E31, 0x4F3D, | # Mushroom Kingdom, WarioWare, Subspace, Rainbow Cruise
 0x503E, 0x513F,	0x5240, 0x5341,	| # Green Greens, Corneria, Venom, Mushroom Kingdom 64
 0x5442, 0x5543, 0x5644, 0x5745,	| # Pokemon Stadium, TEMP, TEMP, Brinstar Depths
-0x5846, 0x5947, 0x5A48, 0x5B49,	| # TEMP, TEMP, TEMP, TEMP
-0x5C4A, 0x5D4B, 0x5E4C, 0x5F4D,	| # TEMP, TEMP, TEMP, TEMP
+0x5846, 0x4124, 0x1915, 0x5B49,	| # TEMP, Mario Circuit, Castle Siege, TEMP
+0x5C4A, 0x5D4B, 0x5E4C, 0x5F4D,	| # TEMP, TEMP, TEMP, Poke Floats
 0x604E, 0x614F, 0x6250, 0x6351,	| # TEMP, TEMP, TEMP, TEMP
 0x6452, 0x6553, 0x6654, 0x6755,	| # TEMP, TEMP, TEMP, TEMP
 0x6856, 0x6957, 0x6A58, 0x6B59,	| # TEMP, TEMP, TEMP, TEMP
